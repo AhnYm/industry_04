6a92febba3a5eba17ee6d3244c0cea8c3001ff2c
AhnYm <dud6450@gmail.com>
2026-03-17 10:26:54 +0900

    loop5_continue

---
+++ basic/loop/loop5_continue.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+
+int main()
+{
+	for (int i = 1; i <= 100; i++)
+	{
+		if (i%3 == 0 || i%5==0)
+		{
+			continue;
+		}
+		printf("%d\n", i);
+	}
+		
+	return 0
+}
